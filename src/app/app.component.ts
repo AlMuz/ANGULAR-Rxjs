7f2de2c456cfdea8bc374ea557fff5246bd1e596
Alexey Muzichenko <almuz@inbox.lv>
2021-04-08 17:06:02 +0300

    simple timer with rxjs stream

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+import { interval } from 'rxjs';
+// import {} from 'rxjs/operators';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ANGULAR-rxjs';
+  constructor() {
+    const intervalStream$ = interval(1000);
+
+    intervalStream$.subscribe((value) => {
+      console.log(value);
+    });
+  }
 }
